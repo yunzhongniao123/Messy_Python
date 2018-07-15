5181a4317bfc93419db957ba6b3ec249d6110956
yzh <781743861@qq.com>
2018-07-15 10:20:32 +0800

    add

--- PythonTest/PythonBasic.py
+++ PythonTest/PythonBasic.py
@@ -75,5 +75,16 @@ def fib2(n):
     return fib2(n - 1) + fib2(n - 2)
 
 
+def fib3(n):
+    if n == 1:
+        return [1]
+    if n == 2:
+        return [1, 1]
+    fibs = [1, 1]
+    for i in range(2, n):
+        fibs.append(fibs[-1] + fibs[-2])
+    return fibs
+
+
 if __name__ == '__main__':
-    print fib2(3)
+    print fib3(5)
